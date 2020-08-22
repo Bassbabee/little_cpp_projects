4f8893670449d1874be409fcf405835c2cbd19b6
Maxim Ilyin <makcim510@mail.ru>
2020-08-22 19:54:39 +0300

    something

---
+++ consts_and_namespace/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace constants
+{
+	// inline constexpr is C++17 or newer only
+    inline constexpr double pi { 3.14159 };
+    inline constexpr double avogadro { 6.0221413e23 };
+    inline constexpr double my_gravity { 9.2 };
+}
+
+#endif
